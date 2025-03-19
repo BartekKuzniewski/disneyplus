bed69888ea9a1a693655e58de5a281bedf57ebf5
Bartek <kuzniewski.bar@gmail.com>
2025-03-19 14:53:22 +0100

    added nav for home page

--- src/components/HomePage/Header.jsx
+++ src/components/HomePage/Header.jsx
@@ -1,7 +1,23 @@
+import { Link } from 'react-router-dom';
+import bgSmall from '../../assets/images/homePageBgSmall.webp';
+import logo from '../../assets/images/disneyLogo.png';
+
 export function Header() {
 	return (
-		<header>
-			<h1 className='font-main text-5xl'>disney +</h1>
+		<header
+			className='relative h-[90dvh] bg-cover'
+			style={{ backgroundImage: `url(${bgSmall})` }}
+		>
+			<div className='absolute inset-0 bg-black opacity-50 '></div>
+
+			<nav className='relative z-10 flex flex-row justify-between items-center px-4 py-2'>
+				<Link>
+					<img src={logo} alt='Logo Disney+' className='h-12' />
+				</Link>
+				<Link className='text-white bg-black border border-white px-4 py-2 rounded-md hover:bg-white hover:text-black transition-colors duration-400 ease-in-out md:text-base'>
+					Zaloguj się
+				</Link>
+			</nav>
 		</header>
 	);
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,9 @@
-import { Header } from "../components/HomePage/Header";
+import { Header } from '../components/HomePage/Header';
 
 export function HomePage() {
-	console.log('HomePage component rendered');
-
 	return (
-		<div className='bg-cyan-950 h-dvh'>
-			<Header/>
+		<div>
+			<Header />
 		</div>
 	);
 }
