0b4659b25a4e66a65f2704d66e7dee6a6d44201a
Bartek <kuzniewski.bar@gmail.com>
2025-04-07 16:46:42 +0200

    added static login page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,7 +1,7 @@
 export function RegisterPage() {
     return (
         <div>
-            <h1 className='text-5xl text-red-300'>register page</h1>
+            <h1 className='bg-login'>register page</h1>
         </div>
     );
 }
