a707be870a0ad19201df6f4908498e6db2c98db0
Bartek <kuzniewski.bar@gmail.com>
2025-04-08 08:46:37 +0200

    added static login page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 export function RegisterPage() {
-    return (
-        <div>
-            <h1 className='bg-login'>register page</h1>
-        </div>
-    );
+	return (
+		<div className='bg-radial-[at_20%_95%] from-[#056877] to-[#051828] to-96% h-screen flex flex-col justify-center items-center text-black p-4'>
+			<Link to='/' className='flex items-center justify-center mb-10 '>
+				<img
+					src='/images/disneyLogo.png'
+					alt='Logo Disney+'
+					className='h-18 md:h-24'
+				/>
+			</Link>
+		</div>
+	);
 }
